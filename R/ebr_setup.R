3fca3cd268da8ec7af7fd4d08265f75f28ad7d08
aaron-lohmann <aaronlohmann@mail.de>
2024-09-23 10:47:05 +0200

    add License

--- R/ebr_setup.R
+++ R/ebr_setup.R
@@ -5,18 +5,16 @@
 #'
 #' @param shock_names A vector of shock names on which elasticity bounds are
 #' placed. Note, each shock can be mentioned multiple times.
-#' @param num_variable A vector of variables which are in the numerator.
-#' Should have the same length as shock_names. Each variable refers to one
-#' shock.
-#' @param denom_variable A vector of variables which are in the denominator.
-#' Should have the same length as shock_names. Each variable refers to one
-#' shock.
+#' @param num_variable A vector of variable indicators which are in the numerator.
+#' Must have the same length as shock_names. Order of reference is in order of shock_names.
+#' @param denom_variable A vector of variable indicators which are in the denominator
+#' Must have the same length as shock_names. Order of reference is in order of shock_names.
 #' @param horizon A vector of horizons for which elasticity bound restrictions
 #' should hold. This should have the same length as shock_names.
 #' @param bounds A vector of numerics which specify the elasticity bounds. This
 #' should have the same length as shock_names.
 #'
-#' @return A tibble which contains the elasticity bound restrictions.
+#' @return A list which contains the elasticity bound restrictions.
 #'
 #' @export
 #'
@@ -30,15 +28,14 @@
 #' ebr_kilian_2009_shocknames <- c("Aggregate Demand", "Oil specific Demand")
 #' ebr_kilian_2009_first <- c(varnames_kilian_2009[2], varnames_kilian_2009[2])
 #' ebr_kilian_2009_second <- c(varnames_kilian_2009[3], varnames_kilian_2009[3])
-#'
 #' ebr_kilian_2009_horizon <- c(1, 1)
 #' ebr_kilian_2009_maxbounds <- c(0.0258, 0.0258)
 #'
 #'
 #' ebr_kilian_2009 <- ebr_setup(
 #'   shock_names = ebr_kilian_2009_shocknames,
-#'   first_variable = ebr_kilian_2009_first,
-#'   second_variable = ebr_kilian_2009_second,
+#'   num_variable = ebr_kilian_2009_first,
+#'   denom_variable = ebr_kilian_2009_second,
 #'   horizon = ebr_kilian_2009_horizon,
 #'   bounds = ebr_kilian_2009_maxbounds
 #' )
@@ -46,14 +43,29 @@
 #' ebr_kilian_2009
 #'
 ebr_setup <- function(shock_names = NULL,
-                      first_variable = NULL,
-                      second_variable = NULL,
+                      num_variable = NULL,
+                      denom_variable = NULL,
                       horizon = NULL,
                       bounds = NULL) {
-  # TODO: Include input checks.
+  # Check if all inputs are provided and of the same length
+  if (is.null(shock_names) || is.null(num_variable) || is.null(denom_variable) ||
+      is.null(horizon) || is.null(bounds)) {
+    stop("All input vectors must be provided.")
+  }
 
-  # collect all specified inputs in a list.
-  ebr <- list(shock_names, first_variable, second_variable, horizon, bounds)
+  if (!all(length(shock_names) == length(num_variable),
+           length(num_variable) == length(denom_variable),
+           length(denom_variable) == length(horizon),
+           length(horizon) == length(bounds))) {
+    stop("All input vectors must have the same length.")
+  }
 
-  ebr
+  # Create a list for output
+  list(
+    shock_names,
+    num_variable,
+    denom_variable,
+    horizon,
+    bounds
+  )
 }
